e4f725d0cf2a38491684be2f7cc873437511b0ba
Drissi Adel <leda91@hotmail.fr>
2024-11-27 11:06:19 +0100

    state direction and snake

--- src/components/SnakeGame.js
+++ src/components/SnakeGame.js
@@ -1,17 +1,32 @@
 import React, { useState, useEffect } from 'react';
+import '../styles/_SnakeGame.scss';
 
 // Composant principal du jeu Snake
 export const SnakeGame = () => {
-  const [score, setScore] = useState(0); // État pour le score
+  const [Snake, setSnake] = useState([[10, 10]]); // État pour le serpent
+  const [direction, setDirection] = useState([[-1, 1]]); // État pour le serpent
 
   useEffect(() => {
     // Logique d'initialisation du jeu ou écoute des événements
-  }, []);
+    const gameInterval = setInterval(() => {
+      moveSnake();
+    });
 
+    return () => clearInterval(gameInterval);
+  }, [direction]);
+
+  const moveSnake = () => {
+    const newHead = [
+      Snake[(0, 0)] + direction[0],
+      Snake[(0, 1)] + direction[1],
+    ];
+  };
+
+  //JSX
   return (
     <div className="game-container">
       <h1>Snake Game</h1>
-      <p>Score: {score}</p>
+      <p>Score: {}</p>
       <div className="game-board"></div>
     </div>
   );
