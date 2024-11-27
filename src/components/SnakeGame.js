a55a9f92f62122b34219aac79ce6aaf8d722de17
Drissi Adel <leda91@hotmail.fr>
2024-11-27 14:59:42 +0100

    Conditions for food and score

--- src/components/SnakeGame.js
+++ src/components/SnakeGame.js
@@ -3,8 +3,10 @@ import '../styles/_SnakeGame.scss';
 
 // Composant principal du jeu Snake
 export const SnakeGame = () => {
-  const [Snake, setSnake] = useState([[10, 10]]); // État pour le serpent
+  const [snake, setSnake] = useState([[10, 10]]); // État pour le serpent
   const [direction, setDirection] = useState([[-1, 1]]); // État pour le serpent
+  const [food, setFood] = useState([[5, 5]]); // État pour la nourriture
+  const [score, setScore] = useState([[0, 0]]); // État pour le score du joueur
 
   useEffect(() => {
     // Logique d'initialisation du jeu ou écoute des événements
@@ -13,15 +15,29 @@ export const SnakeGame = () => {
     });
 
     return () => clearInterval(gameInterval);
-  }, [direction]);
+  }, [snake , direction]);
 
   const moveSnake = () => {
     const newHead = [
-      Snake[(0, 0)] + direction[0],
-      Snake[(0, 1)] + direction[1],
+      snake[(0, 0)] + direction[0],
+      snake[(0, 1)] + direction[1],
     ];
   };
 
+  // Vérifie si le serpent arrive a manger la nourriture
+  if (newHead[0][0] === food[0] && newHead[1] === food[1]) {
+    setScore(score + 1); // Augmente le score de + 1 
+    setFood(generateFood)// Génére une nouvelle nourriture 
+  } 
+  else {
+    snake.pop() // Supprime la queu si il n'y a aucunes nourritures
+  }
+
+  // Ajoute une nouvelle tête
+  setSnake([newHead,...snake])
+
+
+  
   //JSX
   return (
     <div className="game-container">
